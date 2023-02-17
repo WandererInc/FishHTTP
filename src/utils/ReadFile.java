accd4301abe710a7f3bfc022822e60b4430de48a
123we97 <123038886+123we97@users.noreply.github.com>
2023-02-17 12:34:10 +0800

    Update ReadFile.java

--- src/utils/ReadFile.java
+++ src/utils/ReadFile.java
@@ -6,33 +6,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Array;
 
-public class ReadFile {
-    private String fileName;
-    private String encode = "UTF-8";
-    public ReadFile(String fileName, String encode) {
-        this.fileName = fileName;
-        this.encode = encode;
+public StringBuilder readFile() {
+    if (!this.fileName.equalsIgnoreCase("/")) {
+        //TODO 读取文件
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fileName));
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                stringBuilder.append(str);
+                stringBuilder.append("\r\n");
+            }
+            return stringBuilder;
+        } catch (FileNotFoundException e) {
+            return new StringBuilder("<h1 align=\"center\">404 Not Found</h1>\r\n);
+        return null;
+    } else {
+        try {
+            return new ReadFile("index.html").readFile();
+        } catch (Exception e) {
+            return new StringBuilder("<h1 align=\"center\">404 Not Found</h1>\r\n);
+        }
     }
-    public ReadFile(String fileName) {
-        this.fileName = fileName;
-    }
-    public ReadFile() {
+}
 
-    }
-
-    public StringBuilder readFile() {
-        if (!this.fileName.equalsIgnoreCase("/")) {
-            //TODO 读取文件
-            try {
-                StringBuilder stringBuilder = new StringBuilder();
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fileName));
-                String str;
-                while ((str = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(str);
-                    stringBuilder.append("\r\n");
-                }
-            } catch (FileNotFoundException e) {
-                //TODO 抛出404错误
                 new Logger(4, "404 Not Found. No such file ", fileName);
                 return new StringBuilder("<h1 align=\"center\">404 Not Found</h1>\r\n<h3 align=\"center\">Fish HTTP Server v1.0 </h3>");
             } catch (IOException e) {
